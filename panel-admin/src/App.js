3b2ad8845cc400e858ae5d4386616de0d03e3962
Nicolas Vasquez Suazo <nvasquezsu@ing.ucsc.cl>
2025-08-06 14:45:54 -0400

    Initialize panel-admin React app
    Set up a new React application using Create React App in the panel-admin directory. Added initial project files, configuration, and dependencies for development and testing.

---
+++ panel-admin/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { InputText } from 'primereact/inputtext'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <InputText></InputText>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
