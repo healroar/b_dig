5edf4b0bee4e8526297bf33c86827be4a21df268
Nicolas Vasquez Suazo <nvasquezsu@ing.ucsc.cl>
2025-08-07 13:52:18 -0400

    pruebas
    pruebas de intentar hacer funcionar reactprime

--- panel-admin/src/App.js
+++ panel-admin/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 
 import { InputText } from 'primereact/inputtext'
+import { Menubar } from 'primereact/menubar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <InputText></InputText>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-column gap-2">
+        <label htmlFor="username">Username</label>
+        <InputText id="username" aria-describedby="username-help" />
+        <small id="username-help">
+            Enter your username to reset your password.
+        </small>
     </div>
   );
 }
